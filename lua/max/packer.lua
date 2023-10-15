ce911db6da328df0941da74014c42bde4074ee86
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-15 12:58:38 +0200

    add telescope plugin

--- lua/max/packer.lua
+++ lua/max/packer.lua
@@ -9,6 +9,17 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Max : Here you add plugins
+  -- MAX :ADD TELESCOPE
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+
+
+
 end)
 
 -- Max: Now we can use :PackerSync
