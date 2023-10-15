1086e2a21ce897428b63d226e5b35d424e627329
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-15 12:47:47 +0200

    Add Packer Plugin Manager

---
+++ lua/max/packer.lua
@@ -0,0 +1,14 @@
+-- Max : Source from this file is https://github.com/wbthomason/packer.nvim
+--
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+end)
+
+-- Max: Now we can use :PackerSync
--- lua/max/remap.lua
+++ lua/max/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
+-- remap the Ex command to space pv
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
