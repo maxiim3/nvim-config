e457388e93a03a81f00f11e7437dc98b2bfdca42
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-15 13:06:31 +0200

    add telescope remap configuration

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,9 @@
+-- Max : Add config from repo https://github.com/nvim-telescope/telescope.nvim
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+-- Max : Add the following bottom to find git files
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- Max : Add the following bottom to grep 
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });	
+end)
