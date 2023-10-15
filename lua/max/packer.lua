862a5f5d67489ef03f0076b85343097ffec67d72
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-15 13:29:44 +0200

    add telecopt config but having issues unsolved with grep_string commad

--- lua/max/packer.lua
+++ lua/max/packer.lua
@@ -6,19 +6,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Max : Here you add plugins
-  -- MAX :ADD TELESCOPE
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-
-
+	-- Max : Here you add plugins
+	-- MAX :ADD TELESCOPE
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
 end)
 
